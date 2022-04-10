0a0365608981905a3fede79f7aeb4b4230cf8f28
michalpetrycki <michalpetrycki@gmail.com>
2022-04-10 17:29:37 +0100

    Initialize product controller

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,13 @@ import App from './app';
 import PostController from '@/resources/post/post.controller';
 import UserController from '@/resources//user/user.controller';
 import HealthCheckController from '@/resources/health-check/health-check.controller';
+import ProductController from '@/resources/product/product.controller';
 
 // validateEnv();
 
 dotenv.config();
 
-const app = new App([new PostController(), new UserController(), new HealthCheckController()], Number(process.env.PORT));
+const app = new App([new PostController(), new UserController(), new HealthCheckController(),
+                    new ProductController()], Number(process.env.PORT));
 
 app.listen();
